e1b472403584a3c0ea10bc328cdc28c9960d1d55
Eric Weiler <ericpweiler@gmail.com>
2016-07-27 14:52:49 -0400

    Stopped the ticker from moving, preparing for custom timer

--- inject.js
+++ inject.js
@@ -1,7 +1,12 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.msg == "video"){
-		var x = document.getElementsByTagName("video").length;
-		sendResponse(x);
+		var video = document.getElementsByTagName("video").item(0);
+		var currentTime = document.getElementsByClassName("ytp-time-current").item(0);
+		var duration = document.getElementsByClassName("ytp-time-duration").item(0);
+		currentTime.outerHTML = "<span1 class='ytp-time-current'>Halt</span>";
+		duration.textContent = "Testing Control of Existing Elements";
+		sendResponse("Executed");
+	
 	}
 });
 
--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
   chrome.tabs.sendMessage(tabs[0].id, {msg: "video"}, function(x){
-  	document.getElementById("status").innerHTML = "Number of video elements in the DOM is: " + x;
+  	document.getElementById("status").innerHTML = "Response: " + x;
   });
 });
 
