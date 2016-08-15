a1ef58632a269b9f172c299f5c243c3573091cb1
Eric Weiler <ericpweiler@gmail.com>
2016-08-15 03:23:02 -0400

    finished GUI, toggle shift feature now syncs with storage

--- inject.js
+++ inject.js
@@ -20,55 +20,78 @@ function formatTime(time){
 
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if(request.msg == "video")
-		sendResponse("Executedpbr");
-});
-
-function initialize(){
-
-	setInterval(function(){
-		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
-	},1000);
-
-	if(!isNaN(video.duration/video.playbackRate)){
-		duration.textContent = formatTime(video.duration/video.playbackRate);
-		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
-	}
-	video.addEventListener("durationchange", function(){
-		if(!isNaN(video.duration/video.playbackRate)){
-			duration.textContent = formatTime(video.duration/video.playbackRate);
-			currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
-		}
-	});
-	video.addEventListener("ratechange", function(){
-		if(!isNaN(video.duration/video.playbackRate)){
-			duration.textContent = formatTime(video.duration/video.playbackRate);
-			currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
-		}
-	});
-}
 
 var video;
 var currentTime;
 var duration;
+var refreshCurrentTime;
 
+function durationchange(){
+	if(!isNaN(video.duration/video.playbackRate)){
+		duration.textContent = formatTime(video.duration/video.playbackRate);
+		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
+	}
+}
+function ratechange(){
+	if(!isNaN(video.duration/video.playbackRate)){
+		duration.textContent = formatTime(video.duration/video.playbackRate);
+		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
+	}
+}
 
-var videoElsReady = setInterval(function(){
-	if(document.getElementsByTagName("video").item(0) != null){
-		if(document.getElementsByClassName("ytp-time-current") != null){
-			if(document.getElementsByClassName("ytp-time-duration") != null){
-				clearInterval(videoElsReady);
-				video = document.getElementsByTagName("video").item(0)
-				currentTime = document.getElementsByClassName("ytp-time-current").item(0);
-				currentTime.outerHTML = "<span1 class='ytp-time-current'></span>";
-				currentTime = document.getElementsByTagName("span1").item(0);
-				duration = document.getElementsByClassName("ytp-time-duration").item(0);
-				initialize();
+function durationShiftOn(){
+	var videoElsReady = setInterval(function(){
+		if(document.getElementsByTagName("video").item(0) != null){
+			if(document.getElementsByClassName("ytp-time-current") != null){
+				if(document.getElementsByClassName("ytp-time-duration") != null){
+					clearInterval(videoElsReady);
+					video = document.getElementsByTagName("video").item(0)
+					currentTime = document.getElementsByClassName("ytp-time-current").item(0);
+					currentTime.outerHTML = "<span1 class='ytp-time-current'></span>";
+					currentTime = document.getElementsByTagName("span1").item(0);
+					duration = document.getElementsByClassName("ytp-time-duration").item(0);
+					
+					clearInterval(refreshCurrentTime);
+					refreshCurrentTime = setInterval(function(){
+						currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
+					},1000);
+
+					if(!isNaN(video.duration/video.playbackRate)){
+						duration.textContent = formatTime(video.duration/video.playbackRate);
+						currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
+					}
+					video.addEventListener("durationchange", durationchange);
+					video.addEventListener("ratechange", ratechange);
+				}
 			}
 		}
-	}
-},100);
+	},100);
+}
+
+function durationShiftOff(){
+	video.removeEventListener("durationchange", durationchange);
+	video.removeEventListener("ratechange", ratechange);
+	duration.textContent = formatTime(video.duration);
+	clearInterval(refreshCurrentTime);
+	refreshCurrentTime = setInterval(function(){
+		currentTime.textContent = formatTime(video.currentTime);
+	},1000);
+}
+
+chrome.storage.sync.get("setting", function(object){
+	if(object.setting === undefined)
+		chrome.storage.sync.set({"setting": true});
+	if(object.setting == true)
+		durationShiftOn();
+});
+
+chrome.storage.onChanged.addListener(function(object, namespace){
+	if(object.setting.newValue == true)
+		durationShiftOn();
+	else
+		durationShiftOff();
+});
+
 
 
 
--- popup.js
+++ popup.js
@@ -1,20 +1,72 @@
-var button = document.getElementById("Save");
-var input = document.getElementById("input");
-var output = document.getElementById("output");
+var button = document.getElementById("toggle");
+var mode;
 
 button.onclick = function(){
-	chrome.storage.sync.set({"setting": input.value}, function(){
+	chrome.storage.sync.get("setting", function(object){
+		if(object.setting == true){
+			chrome.storage.sync.set({"setting": false});
+			button.innerHTML = "Nope";
+			button.style.backgroundColor = "red";
+			button.style.color = "white";
+			mode = false;
+		}
+		else{
+			document.getElementsByTagName('audio')[0].play();
+			chrome.storage.sync.set({"setting": true});
+			button.innerHTML = "Yeah!";
+			button.style.backgroundColor = "#5AE87C";
+			button.style.color = "white";
+			mode = true;
+		}
 	});
 };
 
+button.onmouseover = function(){
+	if(mode == false){
+		button.style.backgroundColor = "#5AE87C";
+		button.style.color = "white";
+	}
+	else{
+		button.style.backgroundColor = "red";
+		button.style.color = "white";
+	}
+}
+
+button.onmouseout = function(){
+	if(mode == true){
+		button.style.backgroundColor = "#5AE87C";
+		button.style.color = "white";
+	}
+	else{
+		button.style.backgroundColor = "red";
+		button.style.color = "white";
+	}
+}
+
 chrome.storage.sync.get("setting", function(object){
-	output.innerHTML = object.setting;
+	if(object.setting === undefined){
+		chrome.storage.sync.set({"setting": true});
+		button.innerHTML = "Yeah!";
+		button.style.backgroundColor = "#5AE87C";
+			button.style.color = "white";
+	}
+	else{
+		if(object.setting == true){
+			button.innerHTML = "Yeah!";
+			button.style.backgroundColor = "#5AE87C";
+			button.style.color = "white";
+			mode = true;
+		}
+		else{
+			button.innerHTML = "Nope";
+			button.style.backgroundColor = "red";
+			button.style.color = "white";
+			mode = false;		
+		}
+	}	
 });
 
-chrome.storage.onChanged.addListener(function(object, namespace){
-	output.innerHTML = object.setting.newValue;
 
-});
 
 
 
