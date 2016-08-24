63e3c7881445a4be3254a6a1453921c0ea71a3ab
Eric Weiler <ericpweiler@gmail.com>
2016-08-24 02:03:56 -0400

    recovered commits, reduced update time to 5s to avoid chrome.storage.set limits, ready to add to store

--- inject.js
+++ inject.js
@@ -1,14 +1,14 @@
-function formatTime(time){
+function formatTime(time){		//format time for video duration field. Takes time in seconds, returns string with time formatted in HH:MM:SS format
 	var formattedTime = "";
 	time = Math.ceil(time);
 	var addZero = false;
-	if(time >= 3600){
-		addZero = true;
+	if(time >= 3600){	
+		addZero = true; //if the video is an hour or longer, remember to pad minutes with zero if the minute value < 10
 		formattedTime += Math.floor(time / 3600);
 		formattedTime += ":";
 		time -= Math.floor(time/3600)*3600;
 	}
-	if(Math.floor(time/60) < 10  && addZero)
+	if(Math.floor(time/60) < 10 && addZero) //if the video is an hour or longer, remember to pad minutes with zero if the minute value < 10
 		formattedTime += "0";
 	formattedTime += Math.floor(time/60);
 	formattedTime += ":";
@@ -19,14 +19,18 @@ function formatTime(time){
 	return formattedTime;
 }
 
-function durationchange(){
+//Formula to make video duration scale with video speed: scaled duration = original duration / playback rate.
+//whenever formatTime() is called it will often be passed video.duration/video.playbackRate to achieve the scaled
+//duration
+
+function durationchange(){		//when video duration changes, a new video has been loaded, print the new video duration
 	if(!isNaN(video.duration/video.playbackRate)){
 		duration.textContent = formatTime(video.duration/video.playbackRate);
 		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
 	}
 }
 
-function ratechange(){
+function ratechange(){			//when video rate changed, print the new video duration and current time based upon new rate
 	if(!isNaN(video.duration/video.playbackRate)){
 		duration.textContent = formatTime(video.duration/video.playbackRate);
 		currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
@@ -34,43 +38,47 @@ function ratechange(){
 }
 
 function durationShiftOn(){
-	var videoElsReady = setInterval(function(){
+	var videoElsReady = setInterval(function(){		
 		if(document.getElementsByTagName("video").item(0) != null){
 			if(document.getElementsByClassName("ytp-time-current") != null){
-				if(document.getElementsByClassName("ytp-time-duration") != null){
-					clearInterval(videoElsReady);
-					currentTime = document.getElementsByClassName("ytp-time-current").item(0);
-					currentTime.outerHTML = "<span1 class='ytp-time-current'></span>";
-					currentTime = document.getElementsByTagName("span1").item(0);
-					duration = document.getElementsByClassName("ytp-time-duration").item(0);
+				if(document.getElementsByClassName("ytp-time-duration") != null){		//test to see if the three elements to be modified exist
+					clearInterval(videoElsReady);										
+					currentTime = document.getElementsByClassName("ytp-time-current").item(0);	//set references
+					currentTime.outerHTML = "<span1 class='ytp-time-current'></span>";			//change tag from <span> to <span1> so that youtube.com loses reference to the current time field and will no longer update it.
+					currentTime = document.getElementsByTagName("span1").item(0);				//reference was lost through the above line, get it again.
+					duration = document.getElementsByClassName("ytp-time-duration").item(0);	//set references
 					
 					clearInterval(refreshCurrentTime);
-					refreshCurrentTime = setInterval(function(){
-						currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
-					},1000);
+					refreshCurrentTime = setInterval(function(){								//define new function to refresh the time.
+						currentTime.textContent = formatTime(video.currentTime/video.playbackRate);	//currentTime/playbackRate will allow the current time to display the proper current time when the video duration is scaled with playbackrate.
+					},1000);																		//a nice side effect is that the current time will tick once per second regardless of play back rate.
 
-					if(!isNaN(video.duration/video.playbackRate)){
-						duration.textContent = formatTime(video.duration/video.playbackRate);
+					if(!isNaN(video.duration/video.playbackRate)){									//on new video load, sometimes the values for playbackrate and/or duration are undefined. Only print values if they are defined
+						duration.textContent = formatTime(video.duration/video.playbackRate);		
 						currentTime.textContent = formatTime(video.currentTime/video.playbackRate);
 					}
-					video.addEventListener("durationchange", durationchange);
-					video.addEventListener("ratechange", ratechange);
+					video.addEventListener("durationchange", durationchange);					//update duration and currentTime when new video loads
+					video.addEventListener("ratechange", ratechange);							//update duration and currentTime when the rate changes
 				}
 			}
 		}
 	},100);
 }
 
-function incrimentTotals(totalType){
+function incrimentTotals(totalType, value){ //This function updates the totals for either "overall" or "timeSaved" in chrome.storage. totalType can either be "overall" or "timeSaved", var value is only used for totalType = "timeSaved".
 	if(totalType == "overall"){
 		chrome.storage.sync.get("overall", function(object){
 			if(object.overall === undefined)
-				chrome.storage.sync.set({"overall": 0})
-			chrome.storage.sync.set({"overall": (object.overall + 1)});
+				chrome.storage.sync.set({"overall": 0});
+			chrome.storage.sync.set({"overall": (object.overall + value)});
 		});
 	}
-	if(totalType == "timeSaved"){
-
+	if(totalType == "timeSaved" && value > 0){
+		chrome.storage.sync.get("timeSaved", function(object){
+			if(object.timeSaved === undefined)
+				chrome.storage.sync.set({"timeSaved": 0});
+			chrome.storage.sync.set({"timeSaved": (object.timeSaved  + value)});
+		});
 	}
 }
 
@@ -79,59 +87,99 @@ function durationShiftOff(){
 	video.removeEventListener("ratechange", ratechange);
 	duration.textContent = formatTime(video.duration);
 	clearInterval(refreshCurrentTime);
-	refreshCurrentTime = setInterval(function(){
+	refreshCurrentTime = setInterval(function(){					//since the control for updating the current time cannot be given back to youtube.com when the user turns the Duration Setting off, define a new function that does it manually. 
 		currentTime.textContent = formatTime(video.currentTime);
 	},1000);
 }
 
-function setup(){
-	video = document.getElementsByTagName("video").item(0);
-	Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+function setup(){	//
+	video = document.getElementsByTagName("video").item(0); //get <video> reference
+	Object.defineProperty(HTMLMediaElement.prototype, 'playing', {	//define .playing for video elements. Credit: http://stackoverflow.com/questions/6877403/how-to-tell-if-a-video-element-is-currently-playing/6877530
     	get: function(){
         	return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
     	}
 	});
-	chrome.storage.sync.get(["setting", "paused"], function(object){
-		if(object.setting === undefined)
+	chrome.storage.sync.get(["setting", "paused", "savedPaused"], function(object){	//get settings.
+		if(object.setting === undefined)											//on first load set defaults.
 			chrome.storage.sync.set({"setting": true});
 		if(object.paused === undefined){
 			chrome.storage.sync.set({"paused": false});
 			paused = false;
 		}
+		if(object.savedPaused === undefined){
+			chrome.storage.sync.set({"savedPaused": false});
+			savedPaused = false;
+		}
+
 		if(object.paused == true)
 			paused = true;
 		else
 			paused = false;
+		if(object.savedPaused == true)
+			savedPaused = true;
+		else
+			savedPaused = false;
 		if(object.setting == true)
-			durationShiftOn();
+			durationShiftOn();		//if "setting" is true, run durationShiftOn to enable functionality
 	});
-	chrome.storage.onChanged.addListener(function(object, namespace){
+	chrome.storage.onChanged.addListener(function(object, namespace){	//if the user changes the state of the "paused"/"savedPaused"/"setting" value in chrome.storage through the toggle button in the browser action page, store the new value locally so that the update time function wont increment the "overall" and "timeSaved" values in chrome.storage
 		if(object.paused.newValue == true)
 			paused = true;
 		if(object.paused.newValue == false)
 			paused = false;
 	});
-	chrome.storage.onChanged.addListener(function(object, namespace){
+	chrome.storage.onChanged.addListener(function(object, namespace){ //^^
+		if(object.savedPaused.newValue == true)
+			savedPaused = true;
+		if(object.savedPaused.newValue == false)
+			savedPaused = false;
+	});
+	chrome.storage.onChanged.addListener(function(object, namespace){ //^^
 		if(object.setting.newValue == true)
 			durationShiftOn();
 		if(object.setting.newValue == false)
 			durationShiftOff();
 	});
-	var trackTime = setInterval(function(){
-		if(video.playing && !paused)
-			incrimentTotals("overall");
+	var trackTime = setInterval(function(){		//so long as the video is playing and the user has the settings for overall time and saved time enabled, increment the values.
+		chrome.runtime.sendMessage({message: "You There?"}, function(response) {
+ 			if(response == "yes"){
+				if(video.playing && !paused)
+					incrimentTotals("overall", 1);
+				if(video.playing && !savedPaused)
+					incrimentTotals("timeSaved", 1 - (1/video.playbackRate)); //1 - 1/rate yields time saved
+ 			}
+ 			else{
+ 				if(everyFiveSeconds % 5 == 0){
+ 					if(video.playing && !paused)
+						incrimentTotals("overall", 5);
+					if(video.playing && !savedPaused)
+						incrimentTotals("timeSaved", 5 - (5/video.playbackRate));
+ 				}
+ 			}
+ 			everyFiveSeconds++;
+		});	
 	}, 1000);
+	
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+		if(request.message == "opened"){
+			alert("opened");
+		}
+		if(request.message == "closed"){
+			alert("closed");
+		}
+	});
 }
 
-var video;
-var currentTime;
-var duration;
-var refreshCurrentTime;
-var currentTab;
-var paused;
+var video;	//video element reference
+var currentTime;	//current time <span> reference
+var duration;		//duration <span> reference
+var refreshCurrentTime;	//interval function variable
+var paused;	//boolean to store the state of the "paused" variable in chrome.storage so that interval function can reference it without having to call chrome.storage repeatedly
+var savedPaused; //same thing, just for "savedPaused" in chrome.storage
+var everyFiveSeconds = 0;
 
 
-var videoReady = setInterval(function(){
+var videoReady = setInterval(function(){	//when a video element is detected (!= null) run the setup function.
 	if(document.getElementsByTagName("video").item(0) != null){
 		clearInterval(videoReady);
 		setup();
--- popup.js
+++ popup.js
@@ -1,72 +1,88 @@
-var button = document.getElementById("toggle");
-var pauseTotals = document.getElementById("pauseTotals");
-var resetTotals = document.getElementById("resetTotals");
-var pauseSaved = document.getElementById("pauseSaved");
+var button = document.getElementById("toggle");				//Duration shift toggle button
+var pauseTotals = document.getElementById("pauseTotals");	//Total watch time tracking pause button
+var resetTotals = document.getElementById("resetTotals");	//Total watch time tracking reset button
+var pauseSaved = document.getElementById("pauseSaved");		//The next two are the similar just for total time saved tracking
 var resetSaved = document.getElementById("resetSaved");
 
-var Tyears = document.getElementById("Tyears");
+var Tyears = document.getElementById("Tyears");				//Fields for the y,d,h,m,s in Total watch time
 var Tdays = document.getElementById("Tdays"); 
 var Thours = document.getElementById("Thours"); 
 var Tminutes = document.getElementById("Tminutes"); 
 var Tseconds = document.getElementById("Tseconds"); 
 
-var Syears = document.getElementById("Syears"); 
+var Syears = document.getElementById("Syears"); 			//Total saved time
 var Sdays = document.getElementById("Sdays"); 
 var Shours = document.getElementById("Shours"); 
 var Sminutes = document.getElementById("Sminutes"); 
 var Sseconds = document.getElementById("Sseconds"); 
 
-var mode;
+var mode; 	//boolean to represent the state of the Duration Shift button. Used to eliminate calling chrome.storage.get
+			//just for mouseover events that just would change the css and not necesserily change the mode.
 
-function formatTime(time){
-	var formattedTime = "";
-	time = Math.ceil(time);
-	var addZero = false;
-	if(time >= 3600){
-		addZero = true;
-		formattedTime += Math.floor(time / 3600);
-		formattedTime += ":";
-		time -= Math.floor(time/3600)*3600;
-	}
-	if(Math.floor(time/60) < 10  && addZero)
-		formattedTime += "0";
-	formattedTime += Math.floor(time/60);
-	formattedTime += ":";
-	time -= Math.floor(time/60)*60;
-	if(time < 10)
-		formattedTime += "0";
-	formattedTime += time;
-	return formattedTime;
-}
+//Variables stored in chrome.storage:
+//"paused" : boolean to store total watch time tracking setting
+//"savedPaused" : boolean to store saved time saved tracking setting
+//"overall" : stores total watch time in seconds
+//"timeSaved" : stores total time saved in seconds
+//"setting" : boolean to store Duration Shift setting
 
-function formatTotals(seconds){
-	if(seconds >= 3153600){
+function formatTotals(seconds){		//takes time in seconds, formats, and then prints to Total Watch Time field
+	if(seconds >= 3153600){			//no. of seconds in a year
 		Tyears.innerHTML = Math.floor(seconds / 3153600);
 		seconds -= Math.floor(seconds / 3153600)*3153600;
 	}
-	if(seconds >= 86400){
+	else
+		Tyears.innerHTML = 0;
+	if(seconds >= 86400){			//no. of seconds in a day
 		Tdays.innerHTML = Math.floor(seconds / 86400);
 		seconds -= Math.floor(seconds / 86400)*86400;
 	}
-	if(seconds >= 3600){
+	else
+		Tdays.innerHTML = 0;
+	if(seconds >= 3600){			//no. of seconds in an hour
 		Thours.innerHTML = Math.floor(seconds / 3600);
 		seconds -= Math.floor(seconds / 3600)*3600;
 	}
-	if(seconds >= 60){
+	else
+		Thours.innerHTML = 0;
+	if(seconds >= 60){				//minutes
 		Tminutes.innerHTML = Math.floor(seconds / 60);
 		seconds -= Math.floor(seconds / 60)*60;
 	}
-	Tseconds.innerHTML = seconds;
-
-
-	// Syears.innerHTML = "365";
-	// Sdays.innerHTML = "365";
-	// Shours.innerHTML = "365";
-	// Sminutes.innerHTML = "365";
-	// Sseconds.innerHTML = "365";
-	
+	else
+		Tminutes.innerHTML = 0;
+	Tseconds.innerHTML = Math.floor(seconds);
 }
-pauseTotals.onclick = function(){
+
+function formatSaved(seconds){		//takes time in seconds, formats, and then prints to Total Watch Time field
+	if(seconds >= 3153600){
+		Syears.innerHTML = Math.floor(seconds / 3153600);
+		seconds -= Math.floor(seconds / 3153600)*3153600;
+	}
+	else
+		Syears.innerHTML = 0;
+	if(seconds >= 86400){
+		Sdays.innerHTML = Math.floor(seconds / 86400);
+		seconds -= Math.floor(seconds / 86400)*86400;
+	}
+	else
+		Sdays.innerHTML = 0;
+	if(seconds >= 3600){
+		Shours.innerHTML = Math.floor(seconds / 3600);
+		seconds -= Math.floor(seconds / 3600)*3600;
+	}
+	else
+		Shours.innerHTML = 0;
+	if(seconds >= 60){
+		Sminutes.innerHTML = Math.floor(seconds / 60);
+		seconds -= Math.floor(seconds / 60)*60;
+	}
+	else
+		Sminutes.innerHTML = 0;
+	Sseconds.innerHTML = Math.floor(seconds);
+}
+
+pauseTotals.onclick = function(){	//play/pause toggle button, the state is stored as boolean "paused" in storage.
 	chrome.storage.sync.get("paused", function(object){
 		if(object.paused == true){
 			chrome.storage.sync.set({"paused": false});
@@ -74,16 +90,33 @@ pauseTotals.onclick = function(){
 		}
 		else{
 			chrome.storage.sync.set({"paused": true});
-			pauseTotals.innerHTML = "Play";
+			pauseTotals.innerHTML = "Resume";
 		}
 	});
 }
 
 resetTotals.onclick = function(){
-	chrome.storage.sync.set({"overall": 0})
+	chrome.storage.sync.set({"overall": 0});
 }
 
-button.onclick = function(){
+pauseSaved.onclick = function(){
+	chrome.storage.sync.get("savedPaused", function(object){
+		if(object.savedPaused == true){
+			chrome.storage.sync.set({"savedPaused": false});
+			pauseSaved.innerHTML = "Pause";
+		}
+		else{
+			chrome.storage.sync.set({"savedPaused": true});
+			pauseSaved.innerHTML = "Resume";
+		}
+	});
+}
+
+resetSaved.onclick = function(){
+	chrome.storage.sync.set({"timeSaved": 0});
+}
+
+button.onclick = function(){  //Duration shift toggle
 	chrome.storage.sync.get("setting", function(object){
 		if(object.setting == true){
 			chrome.storage.sync.set({"setting": false});
@@ -124,14 +157,20 @@ button.onmouseout = function(){
 	}
 }
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(request.message == "You There?")
+		sendResponse("yes");
+});
+
 chrome.storage.sync.get("setting", function(object){
-	if(object.setting === undefined){
+	if(object.setting === undefined){ //on first load set default value of "setting" to true. Duration Shift will be on.
 		chrome.storage.sync.set({"setting": true});
 		button.innerHTML = "Yeah!";
 		button.style.backgroundColor = "#5AE87C";
-			button.style.color = "white";
+		button.style.color = "white";
+		mode = true;
 	}
-	else{
+	else{ //if extension has been loaded before, get setting and change button css accordingly, red if off, green if on.
 		if(object.setting == true){
 			button.innerHTML = "Yeah!";
 			button.style.backgroundColor = "#5AE87C";
@@ -147,20 +186,39 @@ chrome.storage.sync.get("setting", function(object){
 	}	
 });
 
-chrome.storage.sync.get(["overall", "paused"], function(object){
-	if(object.overall === undefined)
+chrome.storage.sync.get(["overall", "paused", "savedPaused", "timeSaved"], function(object){
+	if(object.overall === undefined)				//assign default values on first load.
 		chrome.storage.sync.set({"overall": 0});
 	if(object.paused === undefined)
 		chrome.storage.sync.set({"paused": false});
-	if(object.paused == false)
-		pauseTotals.innerHTML = "Pause";
-	else
-		pauseTotals.innerHTML = "Play";
-	formatTotals(object.overall);
+
+	if(object.timeSaved === undefined)
+		chrome.storage.sync.set({"timeSaved": 0});
+	if(object.savedPaused === undefined)
+		chrome.storage.sync.set({"savedPaused": false});
+	chrome.storage.sync.get(["overall", "paused", "savedPaused", "timeSaved"], function(object){
+		if(object.paused == false)					//assign correct text for toggle buttons.
+			pauseTotals.innerHTML = "Pause";
+		else
+			pauseTotals.innerHTML = "Resume";
+
+		if(object.savedPaused == false)
+			pauseSaved.innerHTML = "Pause";
+		else
+			pauseSaved.innerHTML = "Resume";
+
+		formatTotals(object.overall);				
+		formatSaved(Math.floor(object.timeSaved));
+	});
+
 });
 
-chrome.storage.onChanged.addListener(function(object, namespace){
+
+
+chrome.storage.onChanged.addListener(function(object, namespace){	//when content script updates chrome.storage values for overall/time saved, print to fields.
 	formatTotals(object.overall.newValue);
 });
-
+chrome.storage.onChanged.addListener(function(object, namespace){
+	formatSaved(Math.floor(object.timeSaved.newValue));
+});
 
